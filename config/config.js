72f52fd080a02135cada92d0dfdf9b9b730ea334
Aygyun Yakub <49846915+achu94@users.noreply.github.com>
2021-10-12 22:12:31 +0200

    double code

--- config/config.js
+++ config/config.js
@@ -2,12 +2,6 @@ var mysql = require("mysql");
 
 const config = {
   PORT: 5001,
-  mysql_pool: mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "mysql",
-  }),
 };
 
 module.exports = config;
